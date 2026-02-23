038bd5c86749ff11a57fafe34b557c2063739c4c
Jaynil Bhalani <bhalanijaynil@gmail.com>
2026-02-23 20:11:35 +0530

    Update Heavy_Deal.py

--- Heavy_Deal.py
+++ Heavy_Deal.py
@@ -18,7 +18,8 @@ cloudinary.config(
 scope = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("abcxyz.json", scope)
+google_creds = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+creds = ServiceAccountCredentials.from_json_keyfile_dict(google_creds, scope)
 client = gspread.authorize(creds)
 
 
@@ -462,4 +463,5 @@ def refundform():
 # ---------- RUN ----------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
